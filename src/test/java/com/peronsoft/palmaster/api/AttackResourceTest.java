725b1e2d8b5a206246139b77ce6a35ff2e91fd03
Rodrigo <morgan2990@gmail.com>
2016-05-12 21:21:07 -0300

    a ver si sale esto

---
+++ src/test/java/com/peronsoft/palmaster/api/AttackResourceTest.java
@@ -0,0 +1,27 @@
+package com.peronsoft.palmaster.api;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AttackResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(AttackResource.class);
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        final String responseMsg = target().path("attackResource").request().get(String.class);
+
+        assertEquals("Hello, Heroku!", responseMsg);
+    }
+}
