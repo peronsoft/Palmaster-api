7483d6723c7a04a1f11209671e7ec3aabda51c8f
Rodrigo <morgan2990@gmail.com>
2016-05-12 23:30:28 -0300

    caca

--- src/main/java/com/peronsoft/palmaster/api/palmaster/service/WeaponService.java
+++ src/main/java/com/peronsoft/palmaster/api/palmaster/service/WeaponService.java
@@ -4,11 +4,14 @@ package com.peronsoft.palmaster.api.palmaster.service;
 import com.peronsoft.palmaster.api.palmaster.model.Weapon;
 import com.peronsoft.palmaster.api.palmaster.parser.WeaponTableReader;
 import jersey.repackaged.com.google.common.collect.Maps;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.Map;
 
 public class WeaponService {
@@ -38,22 +41,20 @@ public class WeaponService {
 
     private Map<String, Weapon> initWeaponMap(){
         Map <String, Weapon> stringWeaponMap = Maps.newHashMap();
-        try {
-            Files.walk(Paths.get(this.getClass().getClassLoader().getResource("weapons").getPath())).forEach(filePath -> {
-                if (Files.isRegularFile(filePath)){
-                    String filename=filePath.getFileName().toString();
-                    try {
-                        Weapon weapon = new WeaponTableReader().readFile(filePath.toString());
-                        stringWeaponMap.put(filename.substring(0, filename.length()-4), weapon);
-                    } catch (URISyntaxException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
+        Path somePath = Paths.get(this.getClass().getClassLoader().getResource("weapons").getPath());
+        String[] extensions = new String[]{"csv"};
+        Iterator<File> iterator = FileUtils.iterateFiles(somePath.toFile(), extensions, false);
+        while(iterator.hasNext()){
+            File weaponFile = iterator.next();
+            Weapon weapon = null;
+            try {
+                weapon = new WeaponTableReader().readFile(weaponFile.getPath());
+                stringWeaponMap.put(weaponFile.getName().substring(0, weaponFile.getName().length()-4), weapon);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return stringWeaponMap;
     }
